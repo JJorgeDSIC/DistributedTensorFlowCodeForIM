a0409b0278f7ef03273acd8281f94e0a9aff2c7c
jjorge <jjorge@dsic.upv.es>
2018-09-11 11:21:39 +0200

    Fixing comma separation between nodes processing

--- resnet/launch_training_dist.sh
+++ resnet/launch_training_dist.sh
@@ -10,9 +10,12 @@ PSLIST=$6
 MASTER=$7
 WLIST=$8
 
+CORRECTEDPSLIT=$(echo $PSLIST | sed -e 's/,/","/g')
+CORRECTEDWLIST=$(echo $WLIST | sed -e 's/,/","/g')
+
 if [ $WNODES -eq 0 ]
 then
-	TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$PSLIST'"]}, "task": {"type": "'$JOB'", "index": 0} }' time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+	export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"]}, "task": {"type": "'$JOB'", "index": 0} }' 
 
 else
 
@@ -21,17 +24,18 @@ else
 	#It is a parameter server, the index is correct
 	if [ "$JOB" == "ps" ]
 	then
-		TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$PSLIST'"], "worker": ["'$WLIST'"]}, "task": {"type": "'$JOB'", "index": '$NODEINDEX'} }' time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+		export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": '$NODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
 
 	else
 		#It is the master node, the index is 0
 		if [ "$JOB" == "master" ]
 		then
-			TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$PSLIST'"], "worker": ["'$WLIST'"]}, "task": {"type": "'$JOB'", "index": 0} }' time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": 0} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
 		else
 			#It is a worker, so the index is NODEINDEX-PSNODES-MNODES
+
 			CORRECTEDNODEINDEX=$(( NODEINDEX - PSNODES - MNODES))
-			TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$PSLIST'"], "worker": ["'$WLIST'"]}, "task": {"type": "'$JOB'", "index": '$CORRECTEDNODEINDEX'} }' time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": '$CORRECTEDNODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
 
 		fi
 
@@ -40,3 +44,5 @@ else
 fi
 
 echo $TF_CONFIG
+
+time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log_"$HOSTNAME".txt 2>&1 &
