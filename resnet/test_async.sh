dc7c081db0f553511c4d97828bf3fcbed124da55
JJorgeDSIC <jjorge@dsic.upv.es>
2018-09-16 21:53:32 +0200

    Debugging...

--- resnet/test_async.sh
+++ resnet/test_async.sh
@@ -12,14 +12,13 @@ WLIST=$8
 CORRECTEDPSLIT=$(echo $PSLIST | sed -e 's/,/","/g')
 CORRECTEDWLIST=$(echo $WLIST | sed -e 's/,/","/g')
 
-
 export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"]}, "task": {"type": "'$JOB'", "index": 0} }' 
 
 echo "Setting paths..."
 
 env > whole_env.txt
 
-export LD_LIBRARY_PATH=/home/ubuntu/src/cntk/bindings/python/cntk/libs:/usr/local/cuda/lib64:/usr/local/lib:/usr/lib:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/mpi/lib:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server
+#export LD_LIBRARY_PATH=/home/ubuntu/src/cntk/bindings/python/cntk/libs:/usr/local/cuda/lib64:/usr/local/lib:/usr/lib:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/mpi/lib:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server
 
 echo $TF_CONFIG > log_env.txt
 
