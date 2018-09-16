5a672633b83c7d89435693ea4241db0da03eb0bf
JJorgeDSIC <jjorge@dsic.upv.es>
2018-09-16 23:23:55 +0200

    Interactive shell does not execute .bashrc, so hard-coded variables are exported here

--- resnet/test_async.sh
+++ resnet/test_async.sh
@@ -12,8 +12,14 @@ WLIST=$8
 CORRECTEDPSLIT=$(echo $PSLIST | sed -e 's/,/","/g')
 CORRECTEDWLIST=$(echo $WLIST | sed -e 's/,/","/g')
 
-source ~/.bashrc
-
+export PATH=/usr/local/cuda/bin:/usr/local/bin:/opt/aws/bin:/home/ubuntu/src/cntk/bin:/usr/local/mpi/bin:$PATH
+export LD_LIBRARY_PATH=/home/ubuntu/src/cntk/bindings/python/cntk/libs:/usr/local/cuda/lib64:/usr/local/lib:/usr/lib:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/mpi/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export PYTHONPATH=/home/ubuntu/src/cntk/bindings/python
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HDFS_HOME=/opt/hadoop-2.9.0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVA_HOME}/jre/lib/amd64/server
+export CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob)
 export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"]}, "task": {"type": "'$JOB'", "index": 0} }' 
 
 echo "Setting paths..."
