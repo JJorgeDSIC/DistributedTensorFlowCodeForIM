4ab27fb9fc99671c5ddbee5b4a09f81fa31b8413
JJorgeDSIC <jjorge@dsic.upv.es>
2018-09-17 19:36:52 +0200

    Refactoring seriously

--- resnet/launch_training_dist_async.sh
+++ resnet/launch_training_dist_async.sh
@@ -6,13 +6,81 @@ MODELPATH=$3
 TRAINSTEPS=$4
 JOB=$5
 PSLIST=$6
-MASTER=$7
-WLIST=$8
-PSNODES=$9
-WNODES=$10
+WLIST=$7
 
-CORRECTEDPSLIT=$(echo $PSLIST | sed -e 's/,/","/g')
-CORRECTEDWLIST=$(echo $WLIST | sed -e 's/,/","/g')
+PSNODES=$(echo $PSLIST | awk -F, '{ print NF }')
+WNODES=$(echo $WLIST | awk -F, '{ print NF }')
+
+CORRECTEDPSLIST=$(echo $PSLIST | sed -e 's/,/","/g')
+
+echo "============" > log_env.txt
+ 
+echo "$PSLIST" >> log_env.txt
+echo "$WLIST" >> log_env.txt
+
+echo "PS nodes: "$PSNODES >> log_env.txt
+echo "Worker nodes: "$WNODES >> log_env.txt
+
+
+MASTER=$(echo $WLIST | awk -F, '{ print $1 }')
+echo "Master:"$MASTER >> log_env.txt
+SLAVES=$(echo $WLIST | cut -d',' -f2-)
+echo "Slaves:"$SLAVES >> log_env.txt
+CORRECTEDWLIST=$(echo $SLAVES | sed -e 's/,/","/g')
+echo "Corrected Slaves:"$CORRECTEDWLIST >> log_env.txt
+
+NODENAME=$(echo $HOSTNAME | awk -F. '{print $1}')
+NODEINDEX=$(echo $NODENAME| awk -F- '{print $2}')
+
+echo "Nodename: "$NODENAME >> log_env.txt
+echo "Node index: "$NODEINDEX >> log_env.txt
+#It is a parameter server
+if [ "$JOB" == "ps" ]
+then
+	#If there is just one worker, it is the master
+	# The order of the ps is fine: ps-0,ps-1,...,ps-n
+	# We can use the current index
+	if [ $WNODES -eq 1 ]
+	then
+		export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIST'"]}, "task": {"type": "ps", "index": '$NODEINDEX'} }' 
+	else
+		export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIST'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "ps", "index": '$NODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+	fi
+else
+	#Otherwise, it is a worker
+	#If there is just one worker, this is the master
+	if [ $WNODES -eq 1 ]
+	then
+		MASTER=$WLIST
+		export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIST'"]}, "task": {"type": "master", "index": 0} }' 
+	else
+		#More than one worker, deal with it
+		# The index are correlative: ps-0,ps-1,...,ps-n,...,worker-m,worker-m+1,...
+		# We should remove the number of ps from the current index
+		CORRECTEDNODEINDEX=$(( NODEINDEX - PSNODES))
+		
+		#The first worker will be the master
+		if [ "$JOB" == "worker" ] && [ "$CORRECTEDNODEINDEX" == "0" ]
+		then
+			WORKERJOB=master
+			echo "Master: "$MASTER >> log_env.txt
+			echo "Corrected worker index: "$CORRECTEDNODEINDEX >> log_env.txt
+			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIST'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "master", "index": 0} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+		else
+			#The rest are plain workers
+			WORKERJOB=slave
+			#Index correction, removing the master
+			CORRECTEDNODEINDEX=$(( NODEINDEX - PSNODES - 1))
+			echo "Corrected worker index: "$CORRECTEDNODEINDEX >> log_env.txt
+			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIST'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": '$CORRECTEDNODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
+
+		fi
+	fi
+
+
+fi
+
+
 
 
 echo "Setting paths..."
@@ -26,44 +94,23 @@ export HADOOP_HDFS_HOME=/opt/hadoop-2.9.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVA_HOME}/jre/lib/amd64/server
 export CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob)
 
-if [ $WNODES -eq 0 ]
-then
-	export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"]}, "task": {"type": "'$JOB'", "index": 0} }' 
-else
+env >> log_env.txt
 
-	NODENAME=$(echo $HOSTNAME | awk -F. '{print $1}')
-	NODEINDEX=$(echo $NODENAME| awk -F- '{print $2}')
-	#It is a parameter server, the index is correct
-	if [ "$JOB" == "ps" ]
-	then
-		export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": '$NODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
-
-	else
-		#It is the master node, the index is 0
-		if [ "$JOB" == "master" ]
-		then
-			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": 0} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
-		else
-			#It is a worker, so the index is NODEINDEX-PSNODES-MNODES
-
-			CORRECTEDNODEINDEX=$(( NODEINDEX - PSNODES - MNODES))
-			export TF_CONFIG='{ "environment": "cloud", "model_dir": "'$MODELPATH'", "cluster": { "master": ["'$MASTER'"], "ps": ["'$CORRECTEDPSLIT'"], "worker": ["'$CORRECTEDWLIST'"]}, "task": {"type": "'$JOB'", "index": '$CORRECTEDNODEINDEX'} }' #time python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log.txt 2>&1 &
-
-		fi
-
-
-	fi
-fi
-
-env > whole_env.txt
-
-echo $TF_CONFIG > log_env.txt
+echo $TF_CONFIG >> log_env.txt
 
 echo $LD_LIBRARY_PATH >> log_env.txt
 
 if [ "$JOB" == "ps" ]
 then
+	echo "PS running in bg."
 	python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log_"$HOSTNAME"_async.txt 2> err_"$HOSTNAME"_async.txt &
 else
-	python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log_"$HOSTNAME"_async.txt 2> err_"$HOSTNAME"_async.txt 
+	if [ "$WORKERJOB" == "master" ]
+	then
+		echo "Master running async."
+		python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log_"$HOSTNAME"_async.txt 2> err_"$HOSTNAME"_async.txt 
+	else
+		echo "Worker running in bg."
+		python3 cifar10_main.py --data-dir=$DATAPATH --job-dir=$MODELPATH --num-gpus=$NUMGPUS --train-steps=$TRAINSTEPS > log_"$HOSTNAME"_async.txt 2> err_"$HOSTNAME"_async.txt &
+	fi
 fi
